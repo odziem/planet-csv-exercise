e873fa0ff713e92e3f0e711fee7af34043a2c14c
Adam Odziemkowski <adam@rangle.io>
2020-06-01 13:25:38 -0400

    Solution

--- mod.ts
+++ mod.ts
@@ -5,15 +5,15 @@ import { BufReader } from "https://deno.land/std/io/bufio.ts";
 import { pick } from "https://deno.land/x/lodash@4.17.15-es/lodash.js";
 
 interface Planet {
-  [ key : string ] : string
-};
+  [key: string]: string;
+}
 
-async function loadPlanetData() {
+async function loadPlanetDataPeriods() {
   const path = join(".", "kepler_exoplanets_nasa.csv");
 
   const file = await Deno.open(path);
   const bufReader = new BufReader(file);
-  
+
   const result = await parse(bufReader, {
     header: true,
     comment: "#",
@@ -27,26 +27,15 @@ async function loadPlanetData() {
     const stellarRadius = Number(planet["koi_srad"]);
     const stellarMass = Number(planet["koi_smass"]);
 
-    return planet["koi_disposition"] === "CONFIRMED" 
-      && planetaryRadius > 0.5 && planetaryRadius < 1.5
-      && stellarRadius > 0.99 && stellarRadius < 1.01
-      && stellarMass > 0.78 && stellarMass < 1.04;
+    return planet["koi_disposition"] === "CONFIRMED" &&
+      planetaryRadius > 0.5 && planetaryRadius < 1.5 &&
+      stellarRadius > 0.99 && stellarRadius < 1.01 &&
+      stellarMass > 0.78 && stellarMass < 1.04;
   });
 
-  return planets.map((planet) => {
-    return pick(planet, [
-      "kepler_name",
-      "koi_prad",
-      "koi_smass",
-      "koi_srad",
-      "koi_count",
-      "koi_steff"
-    ]);
-  })
+  return planets.map((planet) => Number(planet["koi_period"]));
 }
 
-const newEarths = await loadPlanetData();
-for (const planet of newEarths) {
-  console.log(planet);
-}
-console.log(`${newEarths.length} habitable planets found!`)
+const newEarthPeriods = await loadPlanetDataPeriods();
+const maxPeriod = Math.max(...newEarthPeriods);
+console.log(`${maxPeriod} days was the longest orbital period found!`);
